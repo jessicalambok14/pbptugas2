b2fdaa15cca0cac648a21692f016b3de2104b3e9
jessica.lambok14 <jessica.lambok@ui.ac.id>
2022-09-14 23:52:02 +0700

    push all

--- katalog/views.py
+++ katalog/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from katalog.models import CatalogItem
 
-# TODO: Create your views here.
+
+def show_katalog(request):
+    data_barang_katalog = CatalogItem.objects.all()
+    context = {
+        'nama': 'Jessica Lambok',
+        'npm': '2106654883',
+        'list_barang': data_barang_katalog
+    }
+    return render(request, "katalog.html", context)
