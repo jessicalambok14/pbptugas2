ba0a98a698663af7bf9ee7b910623ecf4a810003
jessica.lambok14 <jessica.lambok@ui.ac.id>
2022-09-22 09:22:54 +0700

    push test

--- mywatchlist/tests.py
+++ mywatchlist/tests.py
@@ -1,3 +1,15 @@
-from django.test import TestCase
+from django.test import TestCase, Client
 
 # Create your tests here.
+class AppTest(TestCase):
+    def test_app_html(self):
+        response = Client().get('https://pbptugas2jessica.herokuapp.com/mywatchlist/html/')
+        self.assertEqual(response.status_code,200)
+
+    def test_app_xml(self):
+        response = Client().get('https://pbptugas2jessica.herokuapp.com/mywatchlist/xml/')
+        self.assertEqual(response.status_code,200)
+
+    def test_app_json(self):
+        response = Client().get('https://pbptugas2jessica.herokuapp.com/mywatchlist/json/')
+        self.assertEqual(response.status_code,200)
